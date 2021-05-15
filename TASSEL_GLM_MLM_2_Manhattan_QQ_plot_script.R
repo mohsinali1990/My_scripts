4b1eeea16630cfb754654dbe9de8f24ef4f5bd00
unknown <mali1990@yahoo.com>
2021-05-15 01:01:00 -0700

    R script for Manhattan and qqplot from TASSEL GWAS outputs

--- TASSEL_GLM_MLM_2_Manhattan_QQ_plot_script.R
+++ TASSEL_GLM_MLM_2_Manhattan_QQ_plot_script.R
@@ -3,23 +3,23 @@ library(dplyr)
 
 # import TASSEL results
 # note 
-GLM_results <- read.table("Tassel_glm_out.txt", header = T, sep = "\t")
+TASSEL_MLM_Out <- read.table("Tassel_glm_out.txt", header = T, sep = "\t")
 
-#traits name
-head(unique(GLM_results$Trait))
+# Number of traits
+head(unique(TASSEL_MLM_Out$Trait))
 
-# note: trait name must be specific
+# note: for each plot trait name must be specificed  
 
 # first trait as example (i.e., EarHT)
-Trait1 <-  GLM_results %>% filter(.$Trait == "EarHT")
+Trait1 <-  TASSEL_MLM_Out %>% filter(.$Trait == "EarHT")
 
 # Bonferroni correction threshold
 nmrk <- nrow(Trait1)
 (GWAS_Bonn_corr_threshold <- -log10(0.05 / nmrk))
- 
+
 # Manhattan plot
 (Mann_plot <- manhattan(
-  GLM_results,
+  TASSEL_MLM_Out,
   chr = "Chr",
   bp = "Pos",
   snp = "Marker",
@@ -32,14 +32,14 @@ nmrk <- nrow(Trait1)
 )
 
 # QQ plot
-QQ_plot <- qq(GLM_results$p)
+QQ_plot <- qq(TASSEL_MLM_Out$p)
 
 # Manhattan and Q-Q plot arranged in 1 rows and 2 columns 
 old_par <- par()
 
 par(mfrow=c(1,2))
 (Mann_plot <- manhattan(
-  GLM_results,
+  TASSEL_MLM_Out,
   chr = "Chr",
   bp = "Pos",
   snp = "Marker",
@@ -50,6 +50,6 @@ par(mfrow=c(1,2))
   suggestiveline = F,
   main = "EarHT" # trait name 
 )
-  )
+)
 
-(QQ_plot <- qq(GLM_results$p,  main = "EarHT" ))
+(QQ_plot <- qq(TASSEL_MLM_Out$p,  main = "EarHT" ))
