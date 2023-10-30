4cd1575c4f10778fe494f8462492591eef442c94
mohsinali1990 <mali1990@yahoo.com>
2023-10-30 18:23:54 +0100

    genetic diversity calcualation from TASSEL results

--- TasselDiversityOut.R
+++ TasselDiversityOut.R
@@ -1,5 +1,5 @@
 # MIT License
-# Copyright (c) [2023] [Mohsin Ali]
+# Copyright (c) 2023 Mohsin Ali
 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
